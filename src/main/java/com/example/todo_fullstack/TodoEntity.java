12a2ab2c9a9df7e1a36146d5f5da71dfd8ad80e8
jaehak26 <jack10829@naver.com>
2022-10-07 02:26:20 +0900

    todo 객체

---
+++ src/main/java/com/example/todo_fullstack/TodoEntity.java
@@ -0,0 +1,17 @@
+package com.example.todo_fullstack;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder            //객체 생성 시 빌더 패턴으로 생성할 수 있게 함
+@NoArgsConstructor  //Arg가 없는 생성자 자동생성
+@AllArgsConstructor //class의 모든 Arg를 갖는 생성자 자동생성
+@Data               //@Getter, @Setter, @RequiredArgsConstructor, @ToString, @EqualsAndHashCode을 한꺼번에 설정
+public class TodoEntity {
+    private String id;      //객체 아이디
+    private String userId;  //사용자 아이디
+    private String title;   //todo 타이틀
+    private boolean done;   //todo를 완료한 경우 - true
+}
--- src/main/java/com/example/todo_fullstack/testLombok.java
+++ src/main/java/com/example/todo_fullstack/testLombok.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-
+//컴파일 시 에러가 없으면 테스트 성공
 @Builder
 @RequiredArgsConstructor
 public class testLombok {
