36e14c9e4b63546a52a1b624b1936f097ea6144c
jaehak26 <jack10829@naver.com>
2022-10-07 01:33:26 +0900

    Initial commit

---
+++ src/main/java/com/example/todo_fullstack/TodoFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoFullstackApplication.class, args);
+	}
+
+}
