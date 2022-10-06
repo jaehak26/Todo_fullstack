f1f28ffa37ed5ec345f8c869a577f69c91681875
jaehak26 <jack10829@naver.com>
2022-10-07 02:09:18 +0900

    lombok test

--- src/main/java/com/example/todo_fullstack/testLombok.java
+++ src/main/java/com/example/todo_fullstack/testLombok.java
@@ -1,5 +1,14 @@
 package com.example.todo_fullstack;
 
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+
+@Builder
+@RequiredArgsConstructor
 public class testLombok {
 
+    @NonNull
+    private String id;
 }
